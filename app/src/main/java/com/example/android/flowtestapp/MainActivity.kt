47bdbdf248890306598feb4ed3158f2f2a6a3ab5
hausberger_jani <hausberger.janos@codeyard.eu>
2021-05-11 12:18:56 +0200

    Base component has been added

--- app/src/main/java/com/example/android/flowtestapp/MainActivity.kt
+++ app/src/main/java/com/example/android/flowtestapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.android.flowtestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.flowtestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setRefreshLayout()
+
+        for (i in provideList()) {
+            setText(i)
+        }
     }
+
+    private fun setRefreshLayout() {
+        binding.swipeRefresh.setOnRefreshListener {
+            binding.swipeRefresh.isRefreshing = false
+        }
+    }
+
+    private fun setText(text: String) {
+        val prev = binding.textView.text.toString()
+        val next = prev + "\n" + text
+        binding.textView.text = next
+    }
+
+    private fun provideList() =
+        listOf("a", "b", "c")
 }
